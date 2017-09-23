0663fe93ace294dcc7c0750c390709a76f5dcc51
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-09-23 15:38:25 +0800

    hell fix

--- index.php
+++ index.php
@@ -17,11 +17,6 @@ $container   = get_theme_mod( 'understrap_container_type' );
 $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
-<?php
-  <div class="header col-md-12 container">
-    <img src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" />
-  </div>
- ?>
 
 
 <?php get_footer(); ?>
