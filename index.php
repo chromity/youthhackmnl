f3cf8ff9a02ee88534d86928613a95e5f5d83c99
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-09-23 15:22:30 +0800

    Added header image

--- index.php
+++ index.php
@@ -17,7 +17,12 @@ $container   = get_theme_mod( 'understrap_container_type' );
 $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
-<? /* INSERT CODE HERE FOR SECTIONS */ ?>
+<?
+  <div class="header col-md-12 container">
+    <img src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" />
+  </div>
+
+ ?>
 
 
 <?php get_footer(); ?>
